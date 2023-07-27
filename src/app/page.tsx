2b10a8260c0e8f44375c05c251b9e37c70363124
Sher <sjanmathong@gmail.com>
2023-07-27 19:28:42 +0700

    fix name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Card from "@/components/Card";
 export default function Home() {
   return (
     <main className="flex min-h-screen  flex-col items-center p-12 ">
-      <div className=" w-4/5    py-10 text-4xl	">MAQE Forum</div>
+      <div className=" w-4/5    py-10 text-4xl	">Sher Forum</div>
       <div className=" w-4/5   text-xl">
         Your current timezone is: Asia/Bangkok
       </div>
